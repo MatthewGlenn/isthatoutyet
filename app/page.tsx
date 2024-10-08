8e387044d9f59cc36e1fa6fe56b8d11dce3424c2
John Poe <rockstarpoe@gmail.com>
2024-10-08 16:54:02 -0400

    Pushes example code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const game = await prisma.game.findFirst();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -47,6 +49,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+        </div>
+        <div>
+          <div>Here&apos;s a game {game?.title}</div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
