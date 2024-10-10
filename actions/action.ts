4bb40949edcbb7c99b81d523215abb1d5559e280
johnpoe007 <61294046+johnpoe007@users.noreply.github.com>
2024-10-10 16:58:20 -0400

    Jp main (#26)
    * Pushing changes to repo
    
    * Fixes docker compose syntax
    
    * Pushes example code
    
    * Adds some initial code for seeding db
    
    * fixing bad merge
    
    * Adds seeding
    
    * Adds initial commit for .net core api
    
    * Fix inital seed run
    
    * Adds getProduct by week
    
    ---------
    
    Co-authored-by: John Poe <john.poe007@gmail.com>

--- actions/action.ts
+++ actions/action.ts
@@ -1,15 +1,37 @@
 "use server";
 
 import prisma from '@/lib/db';
-import { revalidatePath } from 'next/cache';
 
-export async function addGame(formData: FormData) {
-    await prisma.game.create({
-        data: {
-            title: formData.get("title") as string,
-            description: "An arcade shooter",
-        },
+export async function getProductByWeek() {
+    const firstDay = getUpcomingSunday();
+    const lastDay = getLastSunday();
+
+    await prisma.product.findMany({
+        include: {
+            Release: {
+                where: {
+                    releaseDate: {
+                        lte: firstDay,
+                        gte: lastDay
+                    }
+                }
+            }
+        }
     });
+}
 
-    revalidatePath("/");
+function getUpcomingSunday() : Date {
+const date = new Date();
+const today = date.getDate();
+const currentDay = date.getDay();
+const newDate = date.setDate(today - currentDay + 7);
+return new Date(newDate);
+}
+
+  function getLastSunday() : Date{
+const date = new Date();
+const today = date.getDate();
+const currentDay = date.getDay();
+const newDate = date.setDate(today - (currentDay || 7));
+return new Date(newDate);
 }
---
+++ prisma/migrations/20241010202928_/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "productTitle" TEXT NOT NULL,
+    "productType" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Release" (
+    "id" TEXT NOT NULL,
+    "releaseDate" TIMESTAMPTZ(3) NOT NULL,
+    "platform" TEXT NOT NULL,
+    "productType" TEXT NOT NULL,
+    "productTitleId" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL,
+
+    CONSTRAINT "Release_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Release" ADD CONSTRAINT "Release_productTitleId_fkey" FOREIGN KEY ("productTitleId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
