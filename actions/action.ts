2371c12f9247e25a490aee0e7663572ccb047501
John Poe <john.poe007@gmail.com>
2024-10-11 08:41:25 -0400

    Adds working example

--- actions/action.ts
+++ actions/action.ts
@@ -6,32 +6,34 @@ export async function getProductByWeek() {
     const firstDay = getUpcomingSunday();
     const lastDay = getLastSunday();
 
-    await prisma.product.findMany({
+    const games = await prisma.product.findMany({
         include: {
             Release: {
                 where: {
                     releaseDate: {
-                        lte: firstDay,
-                        gte: lastDay
+                        lte: lastDay,
+                        gte: firstDay
                     }
                 }
             }
         }
     });
+    
+    return games;
 }
 
 function getUpcomingSunday() : Date {
-const date = new Date();
-const today = date.getDate();
-const currentDay = date.getDay();
-const newDate = date.setDate(today - currentDay + 7);
-return new Date(newDate);
+    const date = new Date();
+    const today = date.getDate();
+    const currentDay = date.getDay();
+    const newDate = date.setDate(today - currentDay + 7);
+    return new Date(newDate);
 }
 
   function getLastSunday() : Date{
-const date = new Date();
-const today = date.getDate();
-const currentDay = date.getDay();
-const newDate = date.setDate(today - (currentDay || 7));
-return new Date(newDate);
+    const date = new Date();
+    const today = date.getDate();
+    const currentDay = date.getDay();
+    const newDate = date.setDate(today - (currentDay || 7));
+    return new Date(newDate);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-// import prisma from "@/lib/db";
-import React from "react";
+ import React from "react";
+ import { getProductByWeek } from "@/actions/action"
 
 export default async function Home() {
   return (
+    
     <div>
       <h1>Home</h1>
       <h2>Image from public folder</h2>
       <p>Testing</p>
+      <h6>{(await getProductByWeek()).map(x=>x.productTitle)}</h6>
     </div>
   );
 }
